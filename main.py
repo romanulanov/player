073ca145cd2c51a533c29a7819c31afcdb8ea36c
romanulanov <turnsourcream@gmail.com>
2023-09-04 13:54:07 +0400

    added fullsize button

--- main.py
+++ main.py
@@ -1,15 +1,1 @@
 from livereload import Server
-
-
-def rebuild():
-    print(1)
-    
-
-def main():
-    server = Server()
-    server.watch('index.html', rebuild)
-    server.serve()
-
-
-if __name__ == "__main__":
-    main()
